173390c5bfc2b28c82556aa90edbe664a2dc5e04
Tim Hawes <me@timhawes.com>
2023-04-13 16:11:27 +0100

    Multiple updates
    Debian upgrade.
    Set root password.
    Install additional packages.
    Build container on GitHub.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,15 @@
 
 /usr/local/sbin/syslogd.pl >/proc/1/fd/1 & 
 
+if [ -n "$CRYPTED_ROOT_PASSWORD" ]; then
+  usermod -p "$CRYPTED_ROOT_PASSWORD" root
+fi
+
+if [ -n "$EXTRA_PACKAGES" ]; then
+  apt-get update
+  apt-get install -y --no-install-recommends $EXTRA_PACKAGES
+fi
+
 if [ -n "$SSH_GITHUB_USER" ]; then
   useradd -s /bin/bash -m $SSH_GITHUB_USER
   su -c "ssh-import-id-gh $SSH_GITHUB_USER" $SSH_GITHUB_USER
